15302050ad0a13dca8e6707538bd61f758064e28
bach130 <cherif.bachir@laposte.net>
2019-06-30 17:48:10 +0200

    premiere modefication

---
+++ MVCAreasDemo/MVCAreasDemo/Areas/GustUser/GustUserAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MVCAreasDemo.Areas.GustUser
+{
+    public class GustUserAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GustUser";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GustUser_default",
+                "GustUser/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MVCAreasDemo/MVCAreasDemo/Areas/SuperUser/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAreasDemo.Areas.SuperUser.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: SuperUser/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
